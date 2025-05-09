38276436626ecdc6ed338380f3ab2a715b3fffc9
SAbotsup <shahidalam0096@gmail.com>
2025-05-10 01:53:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,8 @@
-# info.py
-OWNER_ID = 5249356814  # Replace with your Telegram User ID
-MONGODB_URL = "mongodb+srv://mojahidalam8092:j5yMFo5g0YhUTkIT@cluster0.ivhm8cu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB connection URL
+import os
 
-# Telegram API credentials (Replace with your actual credentials)
-API_ID = 26513278  # Replace with your API ID from https://my.telegram.org
-API_HASH = "7a3df75c8ed3520737e343e9f049166b"  # Replace with your API Hash from https://my.telegram.org
-BOT_TOKEN = "8057096821:AAEDiRjHWCFpeC2TXA_kyQHeMhQZRUGa8SA"  # Replace with your Bot Token from BotFather
+OWNER_ID = int(os.getenv("OWNER_ID", "5249356814"))
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://mojahidalam8092:j5yMFo5g0YhUTkIT@cluster0.ivhm8cu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
+API_ID = int(os.getenv("API_ID", "26513278"))
+API_HASH = os.getenv("API_HASH", "7a3df75c8ed3520737e343e9f049166b")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8057096821:AAEDiRjHWCFpeC2TXA_kyQHeMhQZRUGa8SA")
