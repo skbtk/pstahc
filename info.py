7b9977627a4a1431cca47319ea2e42d38b6931be
SAbotsup <shahidalam0096@gmail.com>
2025-05-10 01:09:56 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "your_bot_token_here"
+API_ID = "your_api_id_here"
+API_HASH = "your_api_hash_here"
+CHANNELS = ["@your_channel_username1", "@your_channel_username2"]  # Add as many as you like
+OWNER_ID = 123456789  # Replace with your Telegram ID
