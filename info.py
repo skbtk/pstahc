011a585dc0fb72a54526ca0ce5fae05232422eb9
SAbotsup <shahidalam0096@gmail.com>
2025-05-10 23:11:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,3 +6,4 @@ MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://mojahidalam8092:j5yMFo5g0Y
 API_ID = int(os.getenv("API_ID", "26513278"))
 API_HASH = os.getenv("API_HASH", "7a3df75c8ed3520737e343e9f049166b")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8057096821:AAEDiRjHWCFpeC2TXA_kyQHeMhQZRUGa8SA")
+CHANNELS = os.getenv("CHANNELS", "").split()
